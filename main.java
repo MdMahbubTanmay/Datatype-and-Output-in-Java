ab6688724ebd7cc20b5f37d6ca8be023b4922eb1
Md Mahbub Tanmay <129056181+MdMahbubTanmay@users.noreply.github.com>
2025-07-16 10:46:35 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    public static void main( )
+    {
+
+     int i =33;
+     String name = "Mahbub";
+     char c = 'A';
+     float score = 3.14f;
+
+
+
+     System.out.println(name+" "+ i +" "+score);
+
+    }
+
+}
